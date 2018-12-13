123e0cab2ca7d3fcb3199aadc833831135f1fb74
Tom Watson <tom@tomjwatson.com>
2018-12-13 17:30:56 +0100

    initial
    Working loading of index via extension. Can't properly intercept
    requests from extension.

---
+++ src/service-worker.js
@@ -0,0 +1,11 @@
+console.log('Started service worker')
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url)
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request)
+    })
+  )
+})
